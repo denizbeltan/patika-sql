afab6aa438bf04ca696013acd410d76e039d706d
Deniz Beltan <80070870+denizbeltan@users.noreply.github.com>
2022-04-20 20:03:31 +0300

    sixth homework added

---
+++ hw6.sql
@@ -0,0 +1,7 @@
+SELECT ROUND(AVG(rental_rate),2) FROM film;
+SELECT COUNT(*) from film
+WHERE title='C%';
+SELECT MAX(length) FROM film
+WHERE rental_rate=0.99;
+SELECT COUNT(DISTINCT replacement_cost) FROM film
+WHERE length>150;
